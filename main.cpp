5f6b96a656d87826af71b34f13d3266f4b75678d
Daniel Stahl <danny.stahl@gmail.com>
2020-03-01 12:25:39 -0800

    Started project and added required functions

---
+++ main.cpp
@@ -0,0 +1,74 @@
+// Daniel Stahl
+// COSCI 130 HW 2B
+
+#include <string>
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+void show(unsigned n);
+void wohs(unsigned n);
+unsigned ones(unsigned n);
+unsigned zeros(unsigned n);
+unsigned f(unsigned n);
+string addSpace(string& bit);
+
+int main() {
+    show(256+12);
+    wohs(256+12);
+    cout << "ones: " << ones(256+12) << endl;
+    cout << "zeros: " << zeros(256+12) << endl;
+}
+
+void show(unsigned n) {
+    string binary = bitset<32>(n).to_string();
+    cout << addSpace(binary) << endl;
+}
+
+void wohs(unsigned n) {
+    string reverseBinary = bitset<32>(n).to_string();
+    reverse(reverseBinary.begin(), reverseBinary.end());
+    cout << addSpace(reverseBinary) << endl;
+}
+
+unsigned ones(unsigned n) {
+    string binary = bitset<32>(n).to_string();
+    unsigned ret = 0;
+    
+    for (int x = 0; x < binary.size(); x++) {
+        ret = (binary[x] == '1') ? ret + 1 : ret + 0;
+    }
+    
+    return ret;
+}
+
+unsigned zeros(unsigned n) {
+    string binary = bitset<32>(n).to_string();
+    unsigned ret = 0;
+    
+    for (int x = 0; x < binary.size(); x++) {
+        ret = (binary[x] == '0') ? ret + 1 : ret + 0;
+    }
+    
+    return ret;
+}
+
+unsigned f(unsigned n) {
+    
+    
+    
+    return 0;
+}
+
+string addSpace(string& byte) {
+    int x = 8;
+    int y = byte.size();
+    
+    while (x < y) {
+        byte.insert(x, " ");
+        x = x + 9;
+    }
+
+    return byte;
+}
